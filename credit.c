9d08e3bf26909c7870cf39d4b1e13c76213ed4c2
Ben McMylor <62849309+benmcmylor@users.noreply.github.com>
2020-04-06 21:33:10 +0100

    Add files via upload

---
+++ credit.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+//program to perform basic authentication checks on a credit card number
+int main (void)
+{
+    //initialise long integer to store 16 digit card number
+    long cardnum = 0; 
+    //initialise integer to store length of card number
+    int length = 0;
+
+    //launch do while loop to prompt user to input card number
+    do 
+    {
+        do 
+        {
+            //prompt user to enter card number
+            cardnum = get_long("Enter the long number as it appears on the front of your card\nNumber: ");        
+            //calculate the number of digits in the card number
+            length = log10(cardnum) + 1;
+        }
+        //reject inputs longer than 16 digits
+        while (length > 16); 
+    }
+    //reject inputs shorter than 13 digits
+    while (length < 13);
+
+    //check point
+    //printf("Your card number is: %li\n", cardnum);
+    //check point
+    //printf("The length of your card number is: %i\n", length);
+
+    //initialise placeholder variable to find first digits of card number
+    long first = cardnum;
+    //isolate first two digits of the card number
+    while (first >= 99)
+    {
+        //keep dividing by 10. cardnum stored as int. /10 deletes the last digit
+        first /= 10;
+    }
+    //check if correct length for amex card
+    if (length == 15)
+    {
+        //check if syntax matches amex
+        if (first == 34 || first == 37)
+        {
+            printf("AMEX\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
+    }
+    //check if correct length for visa or mastercard
+    else if (length == 16)
+    {
+        //check if syntax matches mastercard
+        if (first >= 51 && first <= 55)
+        {
+            printf("MASTERCARD\n");
+        }
+        //check if syntax matches visa
+        else if (first/10 == 4)
+        {
+            printf("VISA\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
+    }
+    //check if length correct for short visa style and if syntax matches visa
+    else if (length == 13 && first/10 == 4)
+    {
+        printf("VISA\n");
+    }
+    else 
+    {
+        printf("INVALID\n");
+    }
+}
